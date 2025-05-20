c85aaf88f6cf5482ae3ed99654853b60e7aff550
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-05-20 09:00:09 +0600

    add config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,7 +18,7 @@ export default tseslint.config(
             },
         },
         rules: {
-            'no-console': 'error',
+            //'no-console': 'error',
             'dot-notation': 'error',
         },
     },
