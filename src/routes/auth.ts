1fd65278deb63658afb37d0a9cb13ebbc39a6fd1
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-05-21 13:49:08 +0600

    add test for persist user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import { AuthController } from '../controllers/AuthControllers'
+import { UserService } from '../services/userService'
+import { AppDataSource } from '../config/data-source'
+import { User } from '../entity/User'
 
 const router = express.Router()
 
-const authController = new AuthController()
+const userRepository = AppDataSource.getRepository(User)
+const userService = new UserService(userRepository)
+const authController = new AuthController(userService)
 
 router.post('/register', (req, res) => authController.register(req, res))
 
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,25 @@
 import request from 'supertest'
 import app from '../../src/app'
+import { DataSource } from 'typeorm'
+import { AppDataSource } from '../../src/config/data-source'
+import { User } from '../../src/entity/User'
+import { truncateTables } from '../utils'
 
 describe('POST /auth/register', () => {
+    let connection: DataSource
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+
+    beforeEach(async () => {
+        await truncateTables(connection)
+    })
+
+    afterAll(async () => {
+        await connection.destroy()
+    })
+
     describe('Given all fields', () => {
         it('should return the 201 status code', async () => {
             //Arrange
@@ -54,6 +72,14 @@ describe('POST /auth/register', () => {
             await request(app).post('/auth/register').send(userData)
 
             //Assert
+            const userRepository = connection.getRepository(User)
+
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(1)
+            expect(users[0].firstName).toBe(userData.firstName)
+            expect(users[0].lastName).toBe(userData.lastName)
+            expect(users[0].email).toBe(userData.email)
         })
     })
 
