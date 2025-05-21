3ad923918e52ad8478bb5cfd2f42ea9f7076db99
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-05-21 10:55:56 +0600

    format files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,12 @@ export default tseslint.config(
     tseslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs', 'jest.config.ts'],
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.ts',
+        ],
     },
     {
         languageOptions: {
