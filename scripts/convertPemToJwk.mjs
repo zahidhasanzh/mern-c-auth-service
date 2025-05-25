f6b1733d4db1f233eefa99cc8660875ffce7422c
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-05-25 13:35:25 +0600

    format convertPemToJwk.mjs file

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -1,9 +1,8 @@
-import fs from "fs";
-import rsaPemToJwk from "rsa-pem-to-jwk"
+import fs from 'fs'
+import rsaPemToJwk from 'rsa-pem-to-jwk'
 
+const privateKey = fs.readFileSync('./certs/private.pem')
 
-const privateKey = fs.readFileSync("./certs/private.pem")
+const jwk = rsaPemToJwk(privateKey, { use: 'sig' }, 'public')
 
-const jwk = rsaPemToJwk(privateKey, {use: 'sig'}, 'public')
-
-console.log(JSON.stringify(jwk));
+console.log(JSON.stringify(jwk))
