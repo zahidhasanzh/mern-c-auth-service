0b2223d437ddaf815fd04f264279790da38de046
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-05-26 22:51:50 +0600

    add logout endpoint

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -222,4 +222,19 @@ export class AuthController {
             return
         }
     }
+
+    async logout(req: AuthRequest, res: Response, next: NextFunction) {
+        try {
+            await this.tokenService.deleteRefreshToken(Number(req.auth.id))
+            this.logger.info('Refresh has been deleted', { id: req.auth.id })
+            this.logger.info('User has been logged out', { id: req.auth.sub })
+
+            res.clearCookie('accessToken')
+            res.clearCookie('refreshToken')
+
+            res.json({})
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -12,6 +12,7 @@ import { CredentialService } from '../services/credentialService'
 import authenticate from '../middlewares/authenticate'
 import { AuthRequest } from '../types'
 import validateRefreshToken from '../middlewares/validateRefreshToken'
+import parseRefreshToken from '../middlewares/parseRefreshToken'
 
 const router = express.Router()
 
@@ -50,6 +51,14 @@ router.post(
     validateRefreshToken,
     (req: Request, res: Response, next: NextFunction) =>
         authController.refresh(req as AuthRequest, res, next),
+)
+
+router.post(
+    '/logout',
+    authenticate,
+    parseRefreshToken,
+    (req: Request, res: Response, next: NextFunction) =>
+        authController.logout(req as AuthRequest, res, next),
 )
 
 export default router
