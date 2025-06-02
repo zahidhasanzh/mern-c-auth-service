58627b2ea653f7464d6871d9154a4d15e6fb436c
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-02 14:40:26 +0600

    config file change nullish coalescing operator and add logical or operator

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 import path from 'path'
 config({
-    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV ?? 'dev'}`),
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || 'dev'}`),
 })
 
 const {
