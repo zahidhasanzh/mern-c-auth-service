c6f9f51f6b80e6557d3125f874d827e5234b2bc0
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-03 12:17:18 +0600

    add admin default user again remove previous

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,15 @@ import app from './app'
 import { Config } from './config'
 import { AppDataSource } from './config/data-source'
 import logger from './config/logger'
-import { initAdminUser } from './init-admin-user/initAdminUser'
+import { createAdminInit } from './utils/createAdminInit'
 
 const startServer = async () => {
     const PORT = Config.PORT
     try {
-        await AppDataSource.initialize().then(async () => {
-            if (process.env.NODE_ENV !== 'test') {
-                await initAdminUser()
-            }
-        })
+        await AppDataSource.initialize()
+
+        // Admin create call
+        await createAdminInit()
 
         logger.info('Database connected successfully.')
         app.listen(PORT, () => {
---
+++ src/utils/createAdminInit.spec.ts
@@ -0,0 +1,58 @@
+import { AppDataSource } from '../config/data-source'
+import { User } from '../entity/User'
+import { createAdminInit } from './createAdminInit'
+
+jest.mock('../config/data-source.ts', () => ({
+    AppDataSource: {
+        getRepository: jest.fn(),
+    },
+}))
+
+const mockFindOne = jest.fn()
+const mockCreate = jest.fn()
+const mockSave = jest.fn()
+
+beforeEach(() => {
+    jest.clearAllMocks()
+
+    AppDataSource.getRepository = jest.fn().mockImplementation((entity) => {
+        if (entity === User) {
+            return {
+                findOne: mockFindOne,
+                create: mockCreate,
+                save: mockSave,
+            }
+        }
+    })
+
+    process.env.ADMIN_EMAIL = 'admin@example.com'
+    process.env.ADMIN_PASSWORD = 'securepassword'
+})
+
+test('should create admin user if not exists', async () => {
+    mockFindOne.mockResolvedValueOnce(null) // No existing user
+    mockCreate.mockReturnValue({ email: 'admin@example.com' })
+
+    await createAdminInit()
+
+    expect(mockCreate).toHaveBeenCalled()
+    expect(mockSave).toHaveBeenCalled()
+})
+
+test('should skip creation if admin already exists', async () => {
+    mockFindOne.mockResolvedValueOnce({ email: 'admin@example.com' })
+
+    await createAdminInit()
+
+    expect(mockCreate).not.toHaveBeenCalled()
+    expect(mockSave).not.toHaveBeenCalled()
+})
+
+test('should log warning if env variables are missing', async () => {
+    process.env.ADMIN_EMAIL = ''
+    process.env.ADMIN_PASSWORD = ''
+
+    await createAdminInit()
+
+    expect(mockCreate).not.toHaveBeenCalled()
+})
