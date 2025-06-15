8b6ef70ebefa9cff837adc580567e8b95ab3ccf9
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-15 14:02:45 +0600

    fix sonar cloud issu

--- src/validators/list-users-validator.ts
+++ src/validators/list-users-validator.ts
@@ -6,14 +6,14 @@ export default checkSchema(
             trim: true,
             customSanitizer: {
                 options: (value: unknown) => {
-                    return value ? value : ''
+                    return value ?? ''
                 },
             },
         },
         role: {
             customSanitizer: {
                 options: (value: unknown) => {
-                    return value ? value : ''
+                    return value ?? ''
                 },
             },
         },
