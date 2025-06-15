3583b86f4ad0a47177e38e07d9cd50b6c3283bd2
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-15 08:54:21 +0600

    import Brackets and improve query search safety

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,4 @@
-import { Repository } from 'typeorm'
+import { Brackets, Repository } from 'typeorm'
 import bcrypt from 'bcryptjs'
 import { User } from '../entity/User'
 import { LimitedUserData, UserData, UserQueryParams } from '../types'
@@ -97,17 +97,17 @@ export class UserService {
     async getAll(validatedQuery: UserQueryParams) {
         const queryBuilder = this.userRepository.createQueryBuilder('user')
 
-        // if (validatedQuery.q) {
-        //     const searchTerm = `%${validatedQuery.q}%`;
-        //     queryBuilder.where(
-        //         new Brackets((qb) => {
-        //             qb.where(
-        //                 "CONCAT(user.firstName, ' ', user.lastName) ILike :q",
-        //                 { q: searchTerm },
-        //             ).orWhere("user.email ILike :q", { q: searchTerm });
-        //         }),
-        //     );
-        // }
+        if (validatedQuery.q) {
+            const searchTerm = `%${validatedQuery.q}%`
+            queryBuilder.where(
+                new Brackets((qb) => {
+                    qb.where(
+                        "CONCAT(user.firstName, ' ', user.lastName) ILike :q",
+                        { q: searchTerm },
+                    ).orWhere('user.email ILike :q', { q: searchTerm })
+                }),
+            )
+        }
 
         if (validatedQuery.role) {
             queryBuilder.andWhere('user.role = :role', {
